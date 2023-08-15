d95887223a0d0defe3013ed25ddcb70bd860dc60
davidthus <dvdnicorici05@gmail.com>
2023-08-15 22:06:43 +0100

    add: markup for notFound component

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,25 @@
+import notFoundAnimation from '@/Assets/Animations/notFoundAnimation.json'
+import { Logo } from '@/Assets/Icons'
+import Lottie from 'lottie-react'
 import Link from 'next/link'
 
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
-    </div>
+    <main>
+      <div className="form__logo">
+        <Logo />
+      </div>
+      <h1 className="form__title headingM">Invoice App</h1>
+      <p className="form__description headingS">
+        Effortlessly manage invoices and payments.
+      </p>
+      <Lottie animationData={notFoundAnimation} />
+      <aside className="not-found__anchor-wrapper">
+        <p className="not-found__anchor--question"></p>
+        <Link href="/" className="not-found__anchor--link">
+          Home.
+        </Link>
+      </aside>
+    </main>
   )
 }
