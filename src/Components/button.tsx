10b65466d773ec055540b821db6005960ad03b6e
davidthus <dvdnicorici05@gmail.com>
2023-08-17 22:54:38 +0100

    add: basic system for styling button component

--- src/Components/button.tsx
+++ src/Components/button.tsx
@@ -1,7 +1,47 @@
 import React from 'react'
 
-function Button() {
-  return <div>B</div>
+export enum BUTTONTYPES {
+  B1,
+  B2,
+  B3,
+  B4,
+  B5,
+  B6
+}
+
+type ButtonProps = {
+  buttonType: 0 | 1 | 2 | 3 | 4 | 5
+  htmlType: 'submit' | 'button' | 'reset'
+  children: React.ReactNode
+}
+
+function Button({ buttonType, htmlType, children }: ButtonProps) {
+  let buttonClass = ''
+  switch (buttonType) {
+    case BUTTONTYPES.B1:
+      buttonClass = 'button1'
+      break
+    case BUTTONTYPES.B2:
+      buttonClass = 'button2'
+      break
+    case BUTTONTYPES.B3:
+      buttonClass = 'button3'
+      break
+    case BUTTONTYPES.B4:
+      buttonClass = 'button4'
+      break
+    case BUTTONTYPES.B5:
+      buttonClass = 'button5'
+      break
+    case BUTTONTYPES.B6:
+      buttonClass = 'button6'
+      break
+  }
+  return (
+    <button type={htmlType} className={`button ${buttonClass ?? ''}`}>
+      {children}
+    </button>
+  )
 }
 
 export default Button
