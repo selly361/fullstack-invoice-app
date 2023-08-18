cc092aded78cbea3c003337723d05cd7c49e42b4
davidthus <dvdnicorici05@gmail.com>
2023-08-18 13:16:13 +0100

    add: markup in sidebar

--- src/Components/sidebar.tsx
+++ src/Components/sidebar.tsx
@@ -1,7 +1,18 @@
+import { Moon, SidebarIcon, Sun } from '@/Assets/Icons'
+
 import React from 'react'
 
 function Sidebar() {
-  return <div>sidebar</div>
+  return (
+    <aside>
+      <SidebarIcon />
+      <div className="sidebar__bottom-group">
+        <Moon />
+        <div className="sidebar__divider" />
+        <div className="sidebar__profile-picture"></div>
+      </div>
+    </aside>
+  )
 }
 
 export default Sidebar
