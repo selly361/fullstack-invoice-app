cd6c35959e103e899117dd587a4c8979e68d0751
davidthus <85802138+davidthus@users.noreply.github.com>
2023-08-21 22:23:53 +0100

    add: styles for main__header element

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,18 +3,17 @@ import Button, { BUTTONTYPES } from "components/button";
 export default function Home() {
   return (
     <main className="main">
-      <header className="main__header">
+      <head er className="main__header--home">
         <aside className="main__info-container">
           <h1 className="main__title headingM">Invoices</h1>
-          <p className="main__info">There are X total invoices</p>
+          <p className="main__info bodyL">There are X total invoices</p>
         </aside>
         <section className="main__actions">
-          <div>DROPdown</div>
-          <Button buttonType={BUTTONTYPES.B1} htmlType="button">
-            New Invoice
-          </Button>
+          <div className={bodyL}>Filter by status</div>
+          <Button buttonType={BUTTONTYPES.B1}>New Invoice</Button>
         </section>
-      </header>
+      </head>
+      {/* INVOICE LIST */}
     </main>
   );
 }
