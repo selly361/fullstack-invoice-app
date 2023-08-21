6637aeb2f031a89009ca46f4d1161134239b7249
davidthus <85802138+davidthus@users.noreply.github.com>
2023-08-21 22:06:20 +0100

    add: markup for header portion of the page

--- src/Components/button.tsx
+++ src/Components/button.tsx
@@ -1,5 +1,5 @@
-import { Plus } from '@/Assets/Icons'
-import React from 'react'
+import { Plus } from "@/Assets/Icons";
+import React from "react";
 
 export enum BUTTONTYPES {
   B1,
@@ -7,49 +7,49 @@ export enum BUTTONTYPES {
   B3,
   B4,
   B5,
-  B6
+  B6,
 }
 
 type ButtonProps = {
-  buttonType: 0 | 1 | 2 | 3 | 4 | 5
-  htmlType: 'submit' | 'button' | 'reset'
-  children: React.ReactNode
-}
+  buttonType: 0 | 1 | 2 | 3 | 4 | 5;
+  htmlType: "submit" | "button" | "reset";
+  children: React.ReactNode;
+};
 
-function Button({ buttonType, htmlType, children }: ButtonProps) {
-  let buttonClass = ''
+function Button({ buttonType, htmlType = "button", children }: ButtonProps) {
+  let buttonClass = "";
   const addIcon = (
     <div className="whiteCircle">
       <Plus />
     </div>
-  )
+  );
 
   switch (buttonType) {
     case BUTTONTYPES.B1:
-      buttonClass = 'button1'
-      break
+      buttonClass = "button1";
+      break;
     case BUTTONTYPES.B2:
-      buttonClass = 'button2'
-      break
+      buttonClass = "button2";
+      break;
     case BUTTONTYPES.B3:
-      buttonClass = 'button3'
-      break
+      buttonClass = "button3";
+      break;
     case BUTTONTYPES.B4:
-      buttonClass = 'button4'
-      break
+      buttonClass = "button4";
+      break;
     case BUTTONTYPES.B5:
-      buttonClass = 'button5'
-      break
+      buttonClass = "button5";
+      break;
     case BUTTONTYPES.B6:
-      buttonClass = 'button6'
-      break
+      buttonClass = "button6";
+      break;
   }
   return (
-    <button type={htmlType} className={`button ${buttonClass ?? ''}`}>
+    <button type={htmlType} className={`button ${buttonClass ?? ""}`}>
       {buttonType === BUTTONTYPES.B1 && addIcon}
       {children}
     </button>
-  )
+  );
 }
 
-export default Button
+export default Button;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,20 @@
+import Button, { BUTTONTYPES } from "components/button";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main className="main">
+      <header className="main__header">
+        <aside className="main__info-container">
+          <h1 className="main__title headingM">Invoices</h1>
+          <p className="main__info">There are X total invoices</p>
+        </aside>
+        <section className="main__actions">
+          <div>DROPdown</div>
+          <Button buttonType={BUTTONTYPES.B1} htmlType="button">
+            New Invoice
+          </Button>
+        </section>
+      </header>
+    </main>
+  );
 }
